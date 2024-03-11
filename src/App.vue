432ffc84f3a989c9b4e353a24eb5bb91b0fd9bca
fullzer4 <gabrielpelizzaro@gmail.com>
2024-03-11 17:43:40 -0300

    new

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,19 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+  <nav>
+    <div>
+      <RouterLink to="/servicos"> Servicos </RouterLink>
+      <RouterLink to="/sobre"> Quem Somos </RouterLink>
+    </div>
+    <div>
+      <img src="/logo.png">
+      <RouterLink to="/"> micro floripa </RouterLink>
+    </div>
+    <div>
+      <RouterLink to="/planos"> Planos </RouterLink>
+      <RouterLink to="/contato"> Contato </RouterLink>
+    </div>
+  </nav>
   <RouterView />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,4 +2,23 @@
 </script>
 
 <template>
+  <section>
+    <div>
+      <h2>Computador com problema?</h2>
+      <p>Não apenas consertamos, mas também elevamos o desempenho e a  confiabilidade dos seus dispositivos. Com uma gama completa de serviços  que incluem upgrades e planos de assistência técnica.</p>
+      <div>
+        <div>
+
+        </div>
+        <RouterLink to="/avaliacoes">Ver mais avaliacoes</RouterLink>
+      </div>
+    </div>
+    <div>
+      <img src="/home.png">
+    </div>
+  </section>
+  <section>
+    <RouterLink to="/contato">Entre em contato</RouterLink>
+    <div></div>
+  </section>
 </template>
